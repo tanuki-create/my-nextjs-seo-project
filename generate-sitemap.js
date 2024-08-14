0143e142a2136869ff6043ba0cc65cb3b9da6087
tanuki-create <guitar.mhyl3@gmail.com>
2024-08-14 11:29:46 +0900

    Fixed bug4

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -6,7 +6,7 @@ import { getAllArticleSlugs } from './src/lib/api.js';
 async function generateSitemap() {
   const smStream = new SitemapStream({ hostname: 'https://keibi.org' });
 
-  const slugs = getAllArticleSlugs();
+  const slugs = await getAllArticleSlugs();
   slugs.forEach((slug) => {
     smStream.write({ url: `/articles/${slug}`, changefreq: 'daily', priority: 0.9 });
   });
@@ -15,7 +15,7 @@ async function generateSitemap() {
 
   const sitemap = await streamToPromise(smStream).then((data) => data.toString());
 
-  fs.writeFileSync(path.join(__dirname, 'public', 'sitemap.xml'), sitemap);
+  fs.writeFileSync(path.join(process.cwd(), 'public', 'sitemap.xml'), sitemap);
 }
 
 generateSitemap();
